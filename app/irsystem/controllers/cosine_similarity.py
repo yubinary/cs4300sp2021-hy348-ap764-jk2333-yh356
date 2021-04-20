65c9fbf3b7eab516358c323b7660af7ecf0aa2b4
Sofia Yoon <hy348@cornell.edu>
2021-04-20 11:49:57 -0400

    Remove nltk.download() and add nltk text file with stopwords

--- app/irsystem/controllers/cosine_similarity.py
+++ app/irsystem/controllers/cosine_similarity.py
@@ -7,7 +7,7 @@ import nltk
 from nltk.tokenize import TreebankWordTokenizer
 from nltk.corpus import stopwords
 
-nltk.download('stopwords')
+# nltk.download('stopwords')
 
 stopwords = set(stopwords.words('english'))
 
