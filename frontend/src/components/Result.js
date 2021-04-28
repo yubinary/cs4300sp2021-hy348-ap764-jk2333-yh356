ce978b9692fe2b2eee280e66fb14bfc933d3e84e
yubinary <yubinheo356@gmail.com>
2021-04-28 03:48:48 -0400

    Connected Flask API to React, PM & WM printed out
    (db commented out)

--- frontend/src/components/Result.js
+++ frontend/src/components/Result.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react';
 
-export default function Result() {
+export default function Result({ pm, wm }) {
   return (
     <div>
-      Result
+      {pm.map(x =>
+        <p>
+          {x}
+        </p>)}
+      {wm.map(x =>
+        <p>
+          {x}
+        </p>)}
     </div>
   );
 }
