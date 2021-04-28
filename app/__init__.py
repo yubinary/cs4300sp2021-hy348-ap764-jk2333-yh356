320e8c10e718b010bcfa09bc513d6827d3ab9216
Ashley Park <ap764@cornell.edu>
2021-04-28 02:02:36 -0400

    merged react again

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,18 @@
 # Gevent needed for sockets
+from app.irsystem import irsystem as irsystem
+from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
+import os
 from gevent import monkey
 monkey.patch_all()
 
 # Imports
-import os
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_socketio import SocketIO
 
 # Configure app
 socketio = SocketIO()
-app = Flask(__name__)
+# app = Flask(__name__)
+app = Flask(__name__, template_folder="build", static_folder="build/static")
 app.config.from_object(os.environ["APP_SETTINGS"])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
@@ -18,13 +20,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 # Import + Register Blueprints
-from app.irsystem import irsystem as irsystem
 app.register_blueprint(irsystem)
 
 # Initialize app w/SocketIO
 socketio.init_app(app)
 
 # HTTP error handling
+
+
 @app.errorhandler(404)
 def not_found(error):
-  return render_template("404.html"), 404
+    return render_template("404.html"), 404
